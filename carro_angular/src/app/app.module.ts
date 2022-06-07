66add45c365d6934a7c6fffd12145552e14574f5
Jose Walter <waltersanta1@hotmail.com>
2022-06-07 09:50:43 -0500

    adicione configuraciones

--- carro_angular/src/app/app.module.ts
+++ carro_angular/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor, ErrorInterceptor } from './helpers';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ carro_angular/src/app/helpers/index.ts
@@ -0,0 +1,3 @@
+export { AuthGuard } from "./auth.guard";
+export { ErrorInterceptor } from "./error.interceptor";
+export { JwtInterceptor } from "./jwt.interceptor";
