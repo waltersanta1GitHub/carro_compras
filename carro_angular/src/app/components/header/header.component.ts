379ca074d77a1e941316f308921608b70f39c656
gabiihacker <gabisantamariam@gmail.com>
2022-06-09 17:39:09 +0200

    mis primeros cambios

--- carro_angular/src/app/components/header/header.component.ts
+++ carro_angular/src/app/components/header/header.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  nombreaplicacion: string = "mi colombia";
+  fecha: string = "";
+  hora: string = "";
   constructor() { }
 
   ngOnInit(): void {
+    this.nombreaplicacion = this.nombreaplicacion.toUpperCase();
+
+    const now = Date.now();
+    this.fecha=now.toString();
+    this.hora=now.toString();
   }
 
 }
+
