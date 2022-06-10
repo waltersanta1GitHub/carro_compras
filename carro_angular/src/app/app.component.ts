07b6c67d0ef4e785a4b1cc05a7090bd618b62c20
Jose Walter <waltersanta1@hotmail.com>
2022-06-10 09:17:16 -0500

    funcionalida basica del side nav terminada

--- carro_angular/src/app/app.component.ts
+++ carro_angular/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CARRITO DE COMPRAS ANGULAR - ';
+  listaCategorias: string[] = [];
+
+  constructor() {
+
+
+  }
+
+  ngOnInit(): void {
+
+    this.listaCategorias =[
+      "Campañas y ofertas",
+      "Informatica","Gaming",
+      "Accesorios de Informatica",
+      "Telefonia",
+      "Television",
+      "Audio y HIFI",
+      "Smart Home"
+    ];
+
+
+  }
+
+
+
+
+
 }
