43f9ad415b47f34e390c6fe9bf24fb278e494fea
Jose Walter <waltersanta1@hotmail.com>
2022-06-16 08:49:18 -0500

    servicios

--- carro_angular/src/app/services/notification.service.ts
+++ carro_angular/src/app/services/notification.service.ts
@@ -1,38 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({ providedIn: 'root' })
 export class AlertService {
-    private subject = new Subject<any>();
-    private keepAfterRouteChange = false;
+  private subject = new Subject<any>();
+  private keepAfterRouteChange = false;
 
-    constructor(private router: Router,private toastr: ToastrService) {
-        // clear alert messages on route change unless 'keepAfterRouteChange' flag is true
-        this.router.events.subscribe(event => {
-            if (event instanceof NavigationStart) {
-                if (this.keepAfterRouteChange) {
-                    // only keep for a single route change
-                    this.keepAfterRouteChange = false;
-                } else {
-                    // clear alert message
-                
-                }
-            }
-        });
-    }
+  constructor(private toastr: ToastrService) {}
 
-    getAlert(): Observable<any> {
-        return this.subject.asObservable();
-    }
-    mostrarMensajeExitoso(message:string, title:string){
-        this.toastr.success(message, title)
-    }
-    
-    mostrarError(message:string, title:string){
-        this.toastr.error(message, title)
-    }
+  getAlert(): Observable<any> {
+    return this.subject.asObservable();
+  }
+  mostrarMensajeExitoso(mensaje: string, titulo: string) {
+    this.toastr.success(mensaje, titulo);
+  }
 
+  mostrarError(mensaje: string, titulo: string) {
+    this.toastr.error(mensaje, titulo);
+  }
 
+  mostrarInformacion(mensaje: string, titulo: string) {
+    this.toastr.info(mensaje, titulo);
+  }
+
+  mostrarAlerta(mensaje: string, titulo: string) {
+    this.toastr.warning(mensaje, titulo);
+  }
 }
